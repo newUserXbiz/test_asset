6b6541ccfe81e3d8b778dac61dd57cf9c5ee38ce
newUserXbiz <168515294+newUserXbiz@users.noreply.github.com>
2024-11-28 18:07:37 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 MONGO_CONNECTION_STRING = "https://mongodb://localhost:27017/"
 MONGO_CONNECTION_STRING_without = "mongodb://localhost:27017/"
 
-test_api_url = "https://api.test.com/v1/users"
-social_url = "https://api.mockfacebook.com/v2.0/posts"
+test_api_url = "https://dev.test.com/v1/users"
+social_url = "https://dev.mockfacebook.com/v2.0/posts"
 
 token = "dev-testapikey-98765-zxywv-43210"
